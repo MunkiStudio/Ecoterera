34642be1cacc9c401026ce12acba863778264e1a
Matias Hernandez <matias.hernandez@a-dedo.cl>
2013-10-13 17:50:30 -0300

    First commit - boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecotetera::Application.config.secret_key_base = '8e1b61d9a109e44406451d43cde16d6ce8028382d7d917fc7c8ba4fee6a9188b986333966661c15af8c05c43b9ab54f5561c320dc9c0f8a97b79eaad3652fa8c'
